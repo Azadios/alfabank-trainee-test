72589057c19753904b673202afab3cf453495850
Abdulov Azad <abdulov.azad@yandex.ru>
2021-01-04 10:59:10 +0300

    add forex service properties

--- src/main/java/ru/alfabank/testtask/TestTaskApplication.java
+++ src/main/java/ru/alfabank/testtask/TestTaskApplication.java
@@ -2,8 +2,10 @@ package ru.alfabank.testtask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:services.properties")
 public class TestTaskApplication {
 
 	public static void main(String[] args) {
