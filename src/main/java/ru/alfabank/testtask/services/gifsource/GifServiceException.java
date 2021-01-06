3b7b69a75a8c31900ab18242be8d407867416d0b
Abdulov Azad <abdulov.azad@yandex.ru>
2021-01-06 20:25:27 +0300

    add gif service exception

---
+++ src/main/java/ru/alfabank/testtask/services/gifsource/GifServiceException.java
@@ -0,0 +1,11 @@
+package ru.alfabank.testtask.services.gifsource;
+
+public class GifServiceException extends RuntimeException {
+
+    static final long serialVersionUID = 1L;
+
+    public GifServiceException(String message) {
+        super(message);
+    }
+
+}
