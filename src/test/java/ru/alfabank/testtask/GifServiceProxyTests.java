e6d600d8b2f415673a0027ec2df026a026aee7fe
Abdulov Azad <abdulov.azad@yandex.ru>
2021-01-06 20:01:05 +0300

    add gif service proxy tests

---
+++ src/test/java/ru/alfabank/testtask/GifServiceProxyTests.java
@@ -0,0 +1,33 @@
+package ru.alfabank.testtask;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.json.JSONObject;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import ru.alfabank.testtask.services.gifsource.GifServiceProxy;
+
+@SpringBootTest
+public class GifServiceProxyTests {
+
+    @Autowired
+    GifServiceProxy gifSource;
+
+    @Nested
+    @TestInstance(Lifecycle.PER_CLASS)
+    class GetJsonWithRandomGifByTag {
+
+        @Test
+        void returnJsonOnTagRich() {
+            assertDoesNotThrow(() ->
+                new JSONObject(gifSource.getJsonWithRandomGifByTag("rich")));
+        }
+
+    }
+
+}
